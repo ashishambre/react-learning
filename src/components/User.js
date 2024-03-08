5ef7582726a74e13ddf720c2b32e5e80713eee4e
Ashish Ambre <a.ambre06@gmail.com>
2024-03-08 13:47:23 +0530

    episode08.02 - Added state variables in class components and functional component

--- src/components/User.js
+++ src/components/User.js
@@ -1,6 +1,12 @@
+import { useState } from "react";
+
 const User = (props) => {
+  const [count] = useState(0);
+  const [count2] = useState(0);
   return (
     <div className="user-card">
+      <h1>Count : {count}</h1>
+      <h1>Count2 : {count2}</h1>
       <h2>Name: {props.name}</h2>
       <h3>Location: Mumbai</h3>
       <h4>Contact: @ashishambre</h4>
