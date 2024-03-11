66ff45e30772c1f8860b42a3ed8e97335524fa85
Ashish Ambre <a.ambre06@gmail.com>
2024-03-11 15:17:37 +0530

    episode 09.01 - Added a custom hook for fetching restaurant menu

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -1,25 +1,12 @@
-import { useEffect, useState } from "react";
 import Shimmer from "./Shimmer";
 import { useParams } from "react-router-dom";
-import { MENU_API } from "../utils/constants";
+import useRestaurantMenu from "../utils/useRestaurantMenu";
 
 const RestaurantMenu = () => {
-  const [resInfo, setResInfo] = useState(null);
+  const {resId} = useParams(); 
 
-  const {resId} = useParams(); // {"resId": "123"}
-
-  // Since we don't want to call useEffect for every render, we will pass an empty array just to call it only once
-  useEffect(() => {
-    fetchMenu();
-  }, []);
-
-  const fetchMenu = async () => {
-    const data = await fetch(MENU_API + resId);
-    const json = await data.json();
-    console.log(json);
-    setResInfo(json.data);
-  };
-
+  const resInfo = useRestaurantMenu(resId);
+  
   if (resInfo === null) {
     return <Shimmer />;
   }
